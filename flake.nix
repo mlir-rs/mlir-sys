9f1201adf8ef66efee9c9a57463d0f2aa90f4359
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-07 17:25:37 -0400

    feat: add nix openssl environment

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A template for rust openssl";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux"
+  ]
+    (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "ssl-project";
+
+          packages = with pkgs; [
+            pkg-config
+            openssl.dev
+            openssl
+          ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      });
+}
